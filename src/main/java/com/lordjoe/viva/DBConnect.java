2cf9e891435b530e6ce1ac046fcb289e84730e2f
lordjoe <HadoopSparkHydra>
2025-04-28 09:32:58 -0700

    no secrets

--- src/main/java/com/lordjoe/viva/DBConnect.java
+++ src/main/java/com/lordjoe/viva/DBConnect.java
@@ -1,8 +1,11 @@
 package com.lordjoe.viva;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * com.lordjoe.viva.DBConnect
@@ -11,14 +14,30 @@ import java.sql.SQLException;
  */
 public class DBConnect {
 
-    private static String USED = "LOCAL";
+    private static String USED = null;
+    private static Properties props = new Properties();
 
+    public static void loadEnv() {
+        try {
+            props.load(new FileInputStream(".env"));
+            props.forEach((k, v) -> System.setProperty(k.toString(), v.toString()));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load .env file", e);
+        }
+    }
 
+    public static String getUsed() {
+        if(USED == null) {
+            loadEnv();
+            USED = props.getProperty("LOCAL");
+        }
+        return USED;
+    }
 
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(USED);
+            return DriverManager.getConnection(getUsed());
         }
         catch(Exception e ){
                      throw  new SQLException(e);
